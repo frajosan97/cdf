3775e93d4534f39d34a3494d33938252080b0bb4
FRANCIS KIOKO <57718524+frajosan97@users.noreply.github.com>
2026-02-23 13:23:48 +0300

    Edited

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            WardSeeder::class,
-            LocationSeeder::class
+            // WardSeeder::class,
+            // LocationSeeder::class
         ]);
     }
 }
